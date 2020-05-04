d85bf1748995d183f7e3e0f73d814787238ed045
Arto Bendiken <arto@bendiken.net>
2020-05-04 17:10:41 +0300

    Disabled the extensions unavailable on Debian 11.

--- lib/openxr/api.rb
+++ lib/openxr/api.rb
@@ -14,20 +14,20 @@ module OpenXR::API
   attach_function :xrCreateAction, [:pointer, :pointer, :pointer], :pointer
   attach_function :xrCreateActionSet, [:pointer, :pointer, :pointer], :pointer
   attach_function :xrCreateActionSpace, [:pointer, :pointer, :pointer], :pointer
-  attach_function :xrCreateDebugUtilsMessengerEXT, [:pointer, :pointer, :pointer], :pointer
+  #attach_function :xrCreateDebugUtilsMessengerEXT, [:pointer, :pointer, :pointer], :pointer
   attach_function :xrCreateInstance, [:pointer, :pointer], :pointer
   attach_function :xrCreateReferenceSpace, [:pointer, :pointer, :pointer], :pointer
   attach_function :xrCreateSession, [:pointer, :pointer, :pointer], :pointer
-  attach_function :xrCreateSpatialAnchorMSFT, [:pointer, :pointer, :pointer], :pointer
-  attach_function :xrCreateSpatialAnchorSpaceMSFT, [:pointer, :pointer, :pointer], :pointer
+  #attach_function :xrCreateSpatialAnchorMSFT, [:pointer, :pointer, :pointer], :pointer
+  #attach_function :xrCreateSpatialAnchorSpaceMSFT, [:pointer, :pointer, :pointer], :pointer
   attach_function :xrCreateSwapchain, [:pointer, :pointer, :pointer], :pointer
   attach_function :xrDestroyAction, [:pointer], :pointer
   attach_function :xrDestroyActionSet, [:pointer], :pointer
-  attach_function :xrDestroyDebugUtilsMessengerEXT, [:pointer], :pointer
+  #attach_function :xrDestroyDebugUtilsMessengerEXT, [:pointer], :pointer
   attach_function :xrDestroyInstance, [:pointer], :pointer
   attach_function :xrDestroySession, [:pointer], :pointer
   attach_function :xrDestroySpace, [:pointer], :pointer
-  attach_function :xrDestroySpatialAnchorMSFT, [:pointer], :pointer
+  #attach_function :xrDestroySpatialAnchorMSFT, [:pointer], :pointer
   attach_function :xrDestroySwapchain, [:pointer], :pointer
   attach_function :xrEndFrame, [:pointer, :pointer], :pointer
   attach_function :xrEndSession, [:pointer], :pointer
@@ -52,31 +52,31 @@ module OpenXR::API
   attach_function :xrGetSystem, [:pointer, :pointer, :pointer], :pointer
   attach_function :xrGetSystemProperties, [:pointer, :ulong_long, :pointer], :pointer
   attach_function :xrGetViewConfigurationProperties, [:pointer, :ulong_long, :pointer, :pointer], :pointer
-  attach_function :xrGetVisibilityMaskKHR, [:pointer, :pointer, :uint32, :pointer, :pointer], :pointer
+  #attach_function :xrGetVisibilityMaskKHR, [:pointer, :pointer, :uint32, :pointer, :pointer], :pointer
   attach_function :xrLocateSpace, [:pointer, :pointer, :long_long, :pointer], :pointer
   attach_function :xrLocateViews, [:pointer, :pointer, :pointer, :uint32, :pointer, :pointer], :pointer
   attach_function :xrPathToString, [:pointer, :ulong_long, :uint32, :pointer, :string], :pointer
-  attach_function :xrPerfSettingsSetPerformanceLevelEXT, [:pointer, :pointer, :pointer], :pointer
+  #attach_function :xrPerfSettingsSetPerformanceLevelEXT, [:pointer, :pointer, :pointer], :pointer
   attach_function :xrPollEvent, [:pointer, :pointer], :pointer
   attach_function :xrReleaseSwapchainImage, [:pointer, :pointer], :pointer
   attach_function :xrRequestExitSession, [:pointer], :pointer
   attach_function :xrResultToString, [:pointer, :pointer, :pointer], :pointer
-  attach_function :xrSessionBeginDebugUtilsLabelRegionEXT, [:pointer, :pointer], :pointer
-  attach_function :xrSessionEndDebugUtilsLabelRegionEXT, [:pointer], :pointer
-  attach_function :xrSessionInsertDebugUtilsLabelEXT, [:pointer, :pointer], :pointer
-  attach_function :xrSetDebugUtilsObjectNameEXT, [:pointer, :pointer], :pointer
-  attach_function :xrSetInputDeviceActiveEXT, [:pointer, :ulong_long, :ulong_long, :uint], :pointer
-  attach_function :xrSetInputDeviceLocationEXT, [:pointer, :ulong_long, :ulong_long, :pointer, :pointer], :pointer
-  attach_function :xrSetInputDeviceStateBoolEXT, [:pointer, :ulong_long, :ulong_long, :uint], :pointer
-  attach_function :xrSetInputDeviceStateFloatEXT, [:pointer, :ulong_long, :ulong_long, :float], :pointer
-  attach_function :xrSetInputDeviceStateVector2fEXT, [:pointer, :ulong_long, :ulong_long, :pointer], :pointer
+  #attach_function :xrSessionBeginDebugUtilsLabelRegionEXT, [:pointer, :pointer], :pointer
+  #attach_function :xrSessionEndDebugUtilsLabelRegionEXT, [:pointer], :pointer
+  #attach_function :xrSessionInsertDebugUtilsLabelEXT, [:pointer, :pointer], :pointer
+  #attach_function :xrSetDebugUtilsObjectNameEXT, [:pointer, :pointer], :pointer
+  #attach_function :xrSetInputDeviceActiveEXT, [:pointer, :ulong_long, :ulong_long, :uint], :pointer
+  #attach_function :xrSetInputDeviceLocationEXT, [:pointer, :ulong_long, :ulong_long, :pointer, :pointer], :pointer
+  #attach_function :xrSetInputDeviceStateBoolEXT, [:pointer, :ulong_long, :ulong_long, :uint], :pointer
+  #attach_function :xrSetInputDeviceStateFloatEXT, [:pointer, :ulong_long, :ulong_long, :float], :pointer
+  #attach_function :xrSetInputDeviceStateVector2fEXT, [:pointer, :ulong_long, :ulong_long, :pointer], :pointer
   attach_function :xrStopHapticFeedback, [:pointer, :pointer], :pointer
   attach_function :xrStringToPath, [:pointer, :string, :pointer], :pointer
   attach_function :xrStructureTypeToString, [:pointer, :pointer, :pointer], :pointer
-  attach_function :xrSubmitDebugUtilsMessageEXT, [:pointer, :ulong_long, :ulong_long, :pointer], :pointer
+  #attach_function :xrSubmitDebugUtilsMessageEXT, [:pointer, :ulong_long, :ulong_long, :pointer], :pointer
   attach_function :xrSuggestInteractionProfileBindings, [:pointer, :pointer], :pointer
   attach_function :xrSyncActions, [:pointer, :pointer], :pointer
-  attach_function :xrThermalGetTemperatureTrendEXT, [:pointer, :pointer, :pointer, :pointer, :pointer], :pointer
+  #attach_function :xrThermalGetTemperatureTrendEXT, [:pointer, :pointer, :pointer, :pointer, :pointer], :pointer
   attach_function :xrWaitFrame, [:pointer, :pointer, :pointer], :pointer
   attach_function :xrWaitSwapchainImage, [:pointer, :pointer], :pointer
 end # OpenXR::API

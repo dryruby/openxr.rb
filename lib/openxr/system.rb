d67d6ed858f39e2e0f9dcb1b98765ecfe8918dcc
Arto Bendiken <arto@bendiken.net>
2020-05-05 12:40:49 +0300

    Implemented System#name, #vendor_id, and properties.

--- lib/openxr/system.rb
+++ lib/openxr/system.rb
@@ -18,21 +18,58 @@ class OpenXR::System
     request[:formFactor] = form_factor.to_i
     system_id = FFI::MemoryPointer.new(XrSystemId)
 
-    # https://www.khronos.org/registry/OpenXR/specs/1.0/man/html/openxr.html#_xrgetsystem3
-    case result = xrGetSystem(instance.handle, request, system_id)
-      when XR_SUCCESS then self.new(system_id.read(XrSystemId))
-      #when XR_ERROR_FORM_FACTOR_UNAVAILABLE then # TODO
-      else raise OpenXR::Error.new(result, :xrGetSystem)
+    begin
+      # https://www.khronos.org/registry/OpenXR/specs/1.0/man/html/openxr.html#_xrgetsystem3
+      case result = xrGetSystem(instance.handle, request, system_id)
+        when XR_SUCCESS then self.new(instance, system_id.read(XrSystemId))
+        #when XR_ERROR_FORM_FACTOR_UNAVAILABLE then # TODO
+        else raise OpenXR::Error.new(result, :xrGetSystem)
+      end
+    ensure
+      system_id.free
     end
   end
 
-  ##
   # @return [API::XrSystemId]
   attr_reader :id
 
+  # @return [String]
+  attr_reader :name
+
+  # @return [Integer]
+  attr_reader :vendor_id
+
+  # @return [Hash<Symbol, Integer>]
+  attr_reader :graphics_properties
+
+  # @return [Hash<Symbol, Bool>]
+  attr_reader :tracking_properties
+
   ##
+  # @param  [Instance] instance
   # @param  [API::XrSystemId, #to_i] id
-  def initialize(id)
+  def initialize(instance, id)
+    @instance = instance
     @id = id.to_i
+
+    response = XrSystemProperties.new
+    response[:base][:type] = XR_TYPE_SYSTEM_PROPERTIES
+
+    # https://www.khronos.org/registry/OpenXR/specs/1.0/man/html/openxr.html#_xrgetsystemproperties3
+    case result = xrGetSystemProperties(@instance.handle, @id, response)
+      when XR_SUCCESS
+        @name = response[:systemName].to_s
+        @vendor_id = response[:vendorId]
+        @graphics_properties = {
+          :max_swapchain_image_height => response[:graphicsProperties][:maxSwapchainImageHeight],
+          :max_swapchain_image_width => response[:graphicsProperties][:maxSwapchainImageWidth],
+          :max_layer_count => response[:graphicsProperties][:maxLayerCount],
+        }
+        @tracking_properties = {
+          :orientation_tracking => response[:trackingProperties][:orientationTracking] == XR_TRUE,
+          :position_tracking    => response[:trackingProperties][:positionTracking] == XR_TRUE,
+        }
+      else raise OpenXR::Error.new(result, :xrGetSystemProperties)
+    end
   end
 end # OpenXR::System

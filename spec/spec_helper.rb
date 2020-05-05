62102454ee9687e453ab1cf31932ed47a6f529e9
Arto Bendiken <arto@bendiken.net>
2020-05-05 09:28:15 +0300

    Configured the OpenXR loader to default to the Monado runtime.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -100,3 +100,6 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+# Configure the OpenXR loader to default to the Monado runtime:
+ENV['XR_RUNTIME_JSON'] ||= '/usr/local/share/openxr/1/openxr_monado.json'

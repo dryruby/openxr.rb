2c22331aae807ae84e8d39b840b185de05c5ecea
Arto Bendiken <arto@bendiken.net>
2020-05-07 07:38:58 +0300

    Defined the Layer class.

--- lib/openxr.rb
+++ lib/openxr.rb
@@ -6,11 +6,18 @@ require_relative 'openxr/api'
 require_relative 'openxr/error'
 require_relative 'openxr/extension'
 require_relative 'openxr/instance'
+require_relative 'openxr/layer'
 require_relative 'openxr/session'
 require_relative 'openxr/system'
 require_relative 'openxr/version'
 
 module OpenXR
+  ##
+  # @return [Boolean]
+  def self.has_layers?
+    nil # TODO
+  end
+
   ##
   # @return [Boolean]
   def self.has_extensions?
---
+++ lib/openxr/layer.rb
@@ -0,0 +1,9 @@
+# This is free and unencumbered software released into the public domain.
+
+require_relative 'api'
+
+##
+# @see https://www.khronos.org/registry/OpenXR/specs/1.0/html/xrspec.html#api-layers
+class OpenXR::Layer
+  # TODO
+end # OpenXR::Layer
